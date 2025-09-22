2e8a3a957a3a7efeb089d266baf5e19eaa6ac21f
sreemuruganSJ <sreemurugansj@gmail.com>
2025-09-23 00:56:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Searching an Element
+
+var hamburger = document.getElementById("hamburger");
+var sidenavbar = document.querySelector(".sidenavbar");
+
+function xmark(){
+    sidenavbar.style.left ='-60%';
+}
+
+function clickhamburger(){
+    sidenavbar.style.left ='0px';
+}
+
+
+
+
+ 
